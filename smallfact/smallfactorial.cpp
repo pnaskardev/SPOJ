1882f821b64c019a82f422397500f57e725b1215
Priyanshu Naskar <71266237+pnaskardev@users.noreply.github.com>
2021-07-30 15:53:35 +0530

    Create smallfactorial.cpp

---
+++ smallfact/smallfactorial.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int number[100];
+int main(void)
+{
+    int test;
+    cin>>test;
+}
+long long factorial(long long integer)
+{
+    int mul=1;
+    for(int i=1;i<=integer;i++)
+    {
+        mul=mul*i;
+    }
+    return (long long)mul;
+}
